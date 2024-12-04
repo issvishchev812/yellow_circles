58969d040ee702617804d84eae89f37cfa972fa4
issvishchev <issvishchev@gmail.com>
2024-12-04 22:33:22 +0300

    scnd cmt

--- .venv/main.py
+++ .venv/main.py
@@ -1,8 +1,9 @@
 import sys
+import random
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QVBoxLayout
 from random import randint
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QWidget
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.uic import loadUi
+from PyQt5.QtCore import Qt
 
 
 class DrawingArea(QWidget):
@@ -12,36 +13,55 @@ class DrawingArea(QWidget):
 
     def paintEvent(self, event):
         painter = QPainter(self)
-        painter.setPen(QColor(255, 255, 0))
 
-        for circle in self.circles:
-            x, y, diameter = circle
+        for color, x, y, diameter in self.circles:
+            painter.setPen(color)
             painter.drawEllipse(x, y, diameter, diameter)
 
     def add_circle(self):
         diameter = randint(10, 100)
         x = randint(0, self.width())
         y = randint(0, self.height())
-        self.circles.append((x, y, diameter))
+        color = QColor(randint(0, 255), randint(0, 255), randint(0, 255))
+        self.circles.append((color, x, y, diameter))
         self.update()
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.resize(400, 300)
+        self.centralwidget = QWidget(MainWindow)
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.pushButton = QPushButton("Draw Circle")
+        self.verticalLayout.addWidget(self.pushButton)
+        MainWindow.setCentralWidget(self.centralwidget)
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.init_ui()
+        self.initialize_ui()
 
-    def init_ui(self):
-        loadUi('UI.ui', self)
-        self.draw_area = DrawingArea(self.centralwidget)
-        self.verticalLayout.addWidget(self.draw_area)
-        self.pushButton.clicked.connect(self.draw_area.add_circle)
+    def initialize_ui(self):
+        self.resize(400, 300)
+        self.setWindowTitle("PyQt Circles")
 
-        self.show()
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+
+        layout = QVBoxLayout(central_widget)
+
+        button = QPushButton("Draw Circle")
+        layout.addWidget(button)
+
+        self.draw_area = DrawingArea()
+        layout.addWidget(self.draw_area)
+
+        button.clicked.connect(self.draw_area.add_circle)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = MainWindow()
-    ex.show()
-    sys.exit(app.exec())
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
